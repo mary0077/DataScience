b8dfbd10cbb8cf367408ca49339e9c85d37ce524
mary0077 <marianagomes201@hotmail.com>
2024-06-09 20:48:13 -0300

    Aulas de datascience

---
+++ ola.py
@@ -0,0 +1,18 @@
+# Exemplo de código Python básico
+
+# Imprime uma mensagem na tela
+print("Olá, mundo!")
+
+# Declara uma variável
+nome = "Mariana"
+
+# Imprime o valor da variável
+print("Olá, " + nome + "!")
+
+# Realiza uma operação matemática
+numero1 = 10
+numero2 = 5
+soma = numero1 + numero2
+
+# Imprime o resultado da operação
+print("A soma de", numero1, "e", numero2, "é igual a", soma)
